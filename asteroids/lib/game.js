e807a93eeb2608809c72db53f023695988fc9187
App Academy Student <student@appacademy.io>
2015-07-27 14:22:15 -0700

    write Game class

---
+++ asteroids/lib/game.js
@@ -0,0 +1,39 @@
+(function() {
+  var Asteroids = window.Asteroids = window.Asteroids || {};
+
+  var Game = Asteroids.Game = function() {
+    this.asteroids = [];
+    this.addAsteroids(Game.NUM_ASTEROIDS);
+  };
+
+  Game.prototype.addAsteroids = function(numAsteroids) {
+    for (var i = 0; i < numAsteroids; i++) {
+      var opts = {};
+      opts.pos = this.randomPos();
+      this.asteroids.push(new Asteroids.Asteroid(opts));
+    }
+  };
+
+  Game.prototype.randomPos = function() {
+    var posX = Math.floor(Math.random() * Game.DIM_X);
+    var posY = Math.floor(Math.random() * Game.DIM_Y);
+    return [posX, posY];
+  };
+
+  Game.prototype.draw = function(ctx) {
+    ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);
+    this.asteroids.forEach(function (asteroid) {
+      asteroid.draw(ctx);
+    });
+  };
+
+  Game.prototype.moveObjects = function() {
+    this.asteroids.forEach(function (asteroid) {
+      asteroid.move();
+    });
+  };
+
+  Game.DIM_X = 500;
+  Game.DIM_Y = 500;
+  Game.NUM_ASTEROIDS = 20;
+})();
