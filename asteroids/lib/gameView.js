70484b943ff39edb40c114a224962fb74477606d
App Academy Student <student@appacademy.io>
2015-07-27 17:34:45 -0700

    add ship movement using Keymaster

--- asteroids/lib/gameView.js
+++ asteroids/lib/gameView.js
@@ -6,7 +6,18 @@
     this.ctx = ctx;
   };
 
+  GameView.prototype.bindKeyHandlers = function() {
+    var theGame = this.game;
+
+    key('w', function () { theGame.ship.power([0, -1]); });
+    key('a', function () { theGame.ship.power([-1, 0]); });
+    key('s', function () { theGame.ship.power([0, 1]); });
+    key('d', function () { theGame.ship.power([1, 0]); });
+  };
+
   GameView.prototype.start = function () {
+    this.bindKeyHandlers();
+
     window.setInterval((function() {
       this.game.step();
       this.game.draw(this.ctx);
--- asteroids/lib/ship.js
+++ asteroids/lib/ship.js
@@ -19,6 +19,10 @@
     this.relocate();
   };
 
+  Ship.prototype.power = function (impulse) {
+    this.vel[0] += impulse[0];
+    this.vel[1] += impulse[1];
+  };
 
   Ship.RADIUS = 10;
   Ship.COLOR = "#994422";
